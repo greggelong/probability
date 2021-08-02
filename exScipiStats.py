18d1fd6787cae625144a6a6ae56699653c0f09bd
Greg Kreisman <gkreisman_cep@qq.com>
2021-08-02 19:28:55 +0800

    Add files via upload

---
+++ exScipiStats.py
@@ -0,0 +1,18 @@
+import scipy.stats as stats
+
+# value of interest
+# change this
+x = 3
+
+# sample size
+# change this
+n = 10
+
+# calculate probability
+prob_1 = stats.binom.pmf(x, n, 0.5)
+print(prob_1)
+
+## Question 2
+## seven heads out of 20 fair coin flips
+prob_2 = stats.binom.pmf(7, 20, 0.5)
+print(prob_2)
